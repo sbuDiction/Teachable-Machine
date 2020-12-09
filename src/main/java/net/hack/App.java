af348dd84d35396915cf64b0fbb15f7a61c969c1
sbuDiction <nsbu68501@gmail.com>
2020-12-09 16:43:11 +0200

    working with on teachable.js :art:

--- src/main/java/net/hack/App.java
+++ src/main/java/net/hack/App.java
@@ -2,9 +2,11 @@ package net.hack;
 
 import net.hack.controller.PlayerController;
 import net.hack.model.Player;
+import net.hack.model.Pose;
 import net.hack.model.Routine;
 import net.hack.routing.AppRouting;
 import net.hack.services.PlayerService;
+import net.hack.services.PoseService;
 import net.hack.services.RoutineService;
 
 import java.util.ArrayList;
@@ -23,6 +25,12 @@ public class App {
 
 //       PlayerService.getInstance().insertPlayer(new Player(1,"Thaabit", "Jacobs", "jacobs@gmail.com"));
 //       RoutineService.getInstance().insertRoutine(new Routine(1,"sbu",new ArrayList<>()));
+//        PoseService.getInstance().insertPose(new Pose(1, "Downward Facing Dog"));
+//        PoseService.getInstance().insertPose(new Pose(2, "Mountain Pose"));
+//        PoseService.getInstance().insertPose(new Pose(3, "High Lunge"));
+//        PoseService.getInstance().insertPose(new Pose(4, "Low Lunge Pose"));
+//        PoseService.getInstance().insertPose(new Pose(5, "Warrior II"));
+//        PoseService.getInstance().insertPose(new Pose(6, "Warrior I"));
     }
 
 }
