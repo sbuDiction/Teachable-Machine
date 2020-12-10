100b97851c3508e640f521960d976fe701409a4c
Mohammed Thaabit Jacobs <mohammedthaabitjacobs@gmail.com>
2020-12-10 11:34:31 +0200

    added port

--- src/main/java/net/hack/App.java
+++ src/main/java/net/hack/App.java
@@ -13,13 +13,22 @@ import net.hack.services.RoutineService;
 
 import java.util.ArrayList;
 
+import static spark.Spark.port;
 import static spark.Spark.staticFiles;
 
 public class App {
 
+    static int getHerokuAssignedPort() {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        if (processBuilder.environment().get("PORT") != null) {
+            return Integer.parseInt(processBuilder.environment().get("PORT"));
+        }
+        return 4567; //return default port if heroku-port isn't set (i.e. on localhost)
+    }
+
 
     public static void main(String[] args) {
-
+        port(getHerokuAssignedPort());
         staticFiles.location("/client");
         /*new AppRouting();*/
 /*
