f67a2cc6c63e0bf04569f2d57035df800f63051c
sbuDiction <nsbu68501@gmail.com>
2020-12-11 08:04:48 +0200

    done working on model :happy:

--- src/main/resources/client/javaScript/models/warrior_i_pose_model.js
+++ src/main/resources/client/javaScript/models/warrior_i_pose_model.js
@@ -1,42 +1,42 @@
-const warriorBtn = document.querySelector('.warrior');
+const musicPlayer = document.getElementById("gameAudio");
 
-// More API functions here:
-// https://github.com/googlecreativelab/teachablemachine-community/tree/master/libraries/pose
+const URL = "https://teachablemachine.withgoogle.com/models/NAH-13YdA/";
+let model, webcam, ctx, labelContainer, maxPredictions;
 
-// the link to your model4 provided by Teachable Machine export panel
-const URL4 = "https://teachablemachine.withgoogle.com/models/NAH-13YdA/";
-let model4, webcam4, ctx4, labelContainer4, maxPredictions4;
+async function init() {
+    const modelURL = URL + "model.json";
+    const metadataURL = URL + "metadata.json";
 
-async function worrior_pose() {
-    const modelURL = URL4 + "model.json";
-    const metadataURL = URL4 + "metadata.json";
-
-    // load the model4 and metadata
+    // load the model and metadata
     // Refer to tmImage.loadFromFiles() in the API to support files from a file picker
     // Note: the pose library adds a tmPose object to your window (window.tmPose)
-    model4 = await tmPose.load(modelURL, metadataURL);
+    model = await tmPose.load(modelURL, metadataURL);
 
-    maxPredictions4 = model4.getTotalClasses();
+    maxPredictions = model.getTotalClasses();
 
-    // Convenience function to setup a webcam4
+    // Convenience function to setup a webcam
     const size = 200;
-    const flip = true; // whether to flip the webcam4
-    webcam4 = new tmPose.Webcam(size, size, flip); // width, height, flip
-    await webcam4.setup(); // request access to the webcam4
-    await webcam4.play();
+    const flip = true; // whether to flip the webcam
+    webcam = new tmPose.Webcam(size, size, flip); // width, height, flip
+    await webcam.setup(); // request access to the webcam
+    await webcam.play();
     window.requestAnimationFrame(loop);
 
     // append/get elements to the DOM
-    const canvas = document.getElementById("canvas4");
+    const canvas = document.getElementById("canvas2");
     canvas.width = size; canvas.height = size;
-    ctx4 = canvas.getContext("2d");
-    labelContainer4 = document.getElementById("label-container");
-    for (let i = 0; i < maxPredictions4; i++) { // and class labels
+    ctx = canvas.getContext("2d");
+    labelContainer = document.getElementById("label-container");
+    for (let i = 0; i < maxPredictions; i++) { // and class labels
+        // labelContainer.appendChild(document.createElement("div"));
     }
+
+
+
 }
 
 async function loop(timestamp) {
-    webcam4.update(); // update the webcam4 frame
+    webcam.update(); // update the webcam frame
     await predict();
     window.requestAnimationFrame(loop);
 }
@@ -46,36 +46,74 @@ async function loop(timestamp) {
 async function predict() {
     // Prediction #1: run input through posenet
     // estimatePose can take in an image, video or canvas html element
-    const { pose, posenetOutput } = await model4.estimatePose(webcam4.canvas);
-    // Prediction 2: run input through teachable machine classification model4
-    const prediction = await model4.predict(posenetOutput);
-    for (let i = 0; i < maxPredictions4; i++) {
+    const { pose, posenetOutput } = await model.estimatePose(webcam.canvas);
+    // Prediction 2: run input through teachable machine classification model
+    const prediction = await model.predict(posenetOutput);
+    let class_ = ""
+    let average = 0;
+    for (let i = 0; i < maxPredictions; i++) {
+
+
+        class_ = prediction[i].className;
+        average = prediction[i].probability.toFixed(2)
         console.log(prediction[i].className);
+
         setTimeout(async () => {
-            if (prediction[i].className === "Mountain Pose") {
-                webcam4.stop();
+            if (prediction[i].className === "Warrior II") {
+
+                musicPlayer.play();
+                // musicPlayer.pause();
+                webcam.stop();
+                const next = document.querySelector('.next');
+                next.classList.remove('hidden')
                 await axios.post("/api/sendUser/", {
                     "name": prediction[i].className,
                     "average": prediction[i].probability.toFixed(2) * 100
                 }).then(response => response.data)
             }
         }, 10000)
+
+
     }
+
+    // poseHandler(class_, average);
+
+    // finally draw the poses
     drawPose(pose);
 }
 
 function drawPose(pose) {
-    if (webcam4.canvas) {
-        ctx4.drawImage(webcam4.canvas, 0, 0);
+    if (webcam.canvas) {
+        ctx.drawImage(webcam.canvas, 0, 0);
         // draw the keypoints and skeleton
         if (pose) {
             const minPartConfidence = 0.5;
-            tmPose.drawKeypoints(pose.keypoints, minPartConfidence, ctx4);
-            tmPose.drawSkeleton(pose.keypoints, minPartConfidence, ctx4);
+            tmPose.drawKeypoints(pose.keypoints, minPartConfidence, ctx);
+            tmPose.drawSkeleton(pose.keypoints, minPartConfidence, ctx);
         }
     }
 }
-warriorBtn.addEventListener('click', () => {
-    console.log("warrior btn clicked");
-    worrior_pose()
-})
+
+
+let href = location.href;
+console.log(href + " href");
+console.log(href === href);
+
+setTimeout(() => {
+    if (href === href) {
+        init();
+        nextSlide();
+
+    }
+}, 5000)
+
+const nextSlide = () => {
+    $('.fetch')
+        .transition({
+            animation: 'scale',
+            duration: '2s',
+            onComplete: () => {
+
+            }
+        })
+}
